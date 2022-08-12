2a6819ad6816c41f01fc0ed74ee5abea0a84deb2
Scott <67354101+scGetStuff@users.noreply.github.com>
2022-08-11 19:40:01 -0500

    renamed files

---
+++ src/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import style from './styles.css';
+
+function doStuff() {
+    const stuff = "stuff";
+
+    console.log(`Doing ${stuff}`);
+}
+
+
+doStuff();
--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// function component() {
-//     const element = document.createElement("div");
-//     element.innerText = "test";
-//     return element;
-// }
-
-// document.body.appendChild(component());
